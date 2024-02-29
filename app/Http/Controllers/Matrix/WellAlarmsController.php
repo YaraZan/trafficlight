6257c7c5d1342d5dd526d8ee3b0872d0432c141a
andrei_potiomkin <89867134953@mail.ru>
2024-02-29 13:45:36 +0300

    add: alarms decrypt column

--- app/Http/Controllers/Matrix/WellAlarmsController.php
+++ app/Http/Controllers/Matrix/WellAlarmsController.php
@@ -25,12 +25,17 @@ class WellAlarmsController extends Controller
             ->join('Well as we', 'fl.Id_Well', '=', 'we.Id')
             ->join('MbReg as mb', 'fl.Id_Reg', '=', 'mb.Id')
             ->join('Ngdu as n', 'we.Ngdu_Id', '=', 'n.Id')
+            ->join('FaultRegPars as frp', function ($join) {
+                $join->on('fl.Id_Reg', '=', 'frp.Reg_Id')
+                    ->whereColumn('frp.Bit', '=', 'fl.Bit');
+            })
             ->select([
                 'fl.Date',
                 'n.Id as Ngdu_Id',
                 'we.Name as WellName',
                 'mb.RegDescript as RegDescript',
                 'fl.Value',
+                'frp.BName as BitName'
             ])
             ->orderBy('fl.Date', 'desc')
             ->where('fl.Id_Well', '=', $well_item->Id)
